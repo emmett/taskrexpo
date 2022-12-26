b97d4ba207c8e65d71320776eea848eb2c733c6d
Emmett Faricy <Emmett@mixpanel.com>
2022-12-25 18:44:24 -0800

    Adding input fields

--- components/addTask.js
+++ components/addTask.js
@@ -1,17 +1,37 @@
-import { View, Text } from 'react-native';
-import Button from '@mui/material/Button';
-import TextField from '@mui/material/TextField';
-import { SafeAreaView } from 'react-native';
+import React from "react";
+import { StyleSheet, View, Text, TextInput } from "react-native";
 
 export default function AddTask() {
   const handleSubmit = () => {
     console.log('Submitted')
   };
 
+  const [text, onChangeText] = React.useState("Useless Text");
+  const [number, onChangeNumber] = React.useState(null);
 
   return (
     <View style={{ flex: 1, alignItems: 'center', justifyContent: 'center' }}>
       <Text>Hello</Text>
+      <TextInput
+        style={styles.input}
+        onChangeText={onChangeText}
+        value={text}
+      />
+      <TextInput
+        style={styles.input}
+        onChangeText={onChangeText}
+        value={text}
+      />
     </View>
   );
 }
+
+const styles = StyleSheet.create({
+  input: {
+    height: 40,
+    margin: 12,
+    borderWidth: 1,
+    padding: 10,
+  },
+});
+
